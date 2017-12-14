bbd8ad598bfb5e6e56a439bfd1c967fe86b4d935
3DEsprit <vilesyn@gmail.com>
2017-12-14 00:02:14 -0600

    setup base app setup without actual functionality

--- App/components/App.js
+++ App/components/App.js
@@ -1,18 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import fetch from 'node-fetch'
 
 export default class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      title: 'Test App'
+      title: 'Test App',
+      counter: 0,
+      progress: 0,
+      loadingList: false,
+      userList: [
+        'none'
+      ]
     }
+  }
+
+  counterUp = () => {
+
+  }
+
+  counterDown = () => {
+
+  }
+
+  getUsers = () => {
+    this.setState({userList: result})
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
+      <div className="container">
+        <div className="body">
+          <h1>{this.state.title}</h1>
+        </div>
+        <div className="body counter">
+          <div className="counter-number">
+            <h1>{this.state.counter}</h1>
+          </div>
+          <button 
+            className="button btn-light"
+          >
+            +
+          </button>
+          <button 
+            className="button btn-light"
+          >
+            -
+          </button>
+        </div>
+        {this.state.progress ?
+        <div className="progress" role="progressbar" aria-valuenow={this.state.progress} aria-valuemin="0" aria-valuetext={`${this.state.progress} percent`} aria-valuemax="100">
+          <div className="progress-meter"></div>
+        </div> :
+        ''}
+        <div className="user-list">
+          <ul>
+            {this.state.userList.map(user => {
+              <li key={user}>{user}</li>
+            })}
+          </ul>
+        </div>
       </div>
     )
   }
